bab9a11957b8c82f05144eb029eb095290e29ad2
BakaErick <khantako3132@gmail.com>
2023-08-26 13:47:01 +0630

    add comment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log("nice to meet you")
+const express = require('express')
+const app = express()
+require('dotenv').config()
 
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function (req, res, next) {
+//   res.send('Hi')
+  //request and respond
+    res.json(
+        {
+            msg: "This is API",
+            data: "hello",
+            status: true
+        }
+    )
+})
+app.listen(PORT, ()=>{
+    console.log(`Server is running on PORT ${PORT}`)
+});
